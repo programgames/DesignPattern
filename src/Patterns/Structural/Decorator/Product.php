78cf9c44d4d5f53ab80b7d6f7ae61c6380e59568
julien <julien@kiboko.fr>
2020-06-04 13:44:47 +0100

    Deployed 90bf747 with MkDocs version: 1.1

---
+++ src/Patterns/Structural/Decorator/Product.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Decorator;
+
+class Product
+{
+    private $name;
+
+    private $price;
+
+    /**
+     * Product constructor.
+     * @param $name
+     * @param $price
+     */
+    public function __construct($name, $price)
+    {
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+}
---
+++ src/Patterns/Structural/Decorator/index.php
@@ -0,0 +1,23 @@
+<?php
+
+require __DIR__ . './../../../../vendor/autoload.php';
+require 'clientCode.php';
+
+use Decorator\BaseCustomer;
+use Decorator\BaseCustomerDecorator;
+use Decorator\Product;
+
+$product = new Product("Iphone","999");
+
+echo "The client code is supporting the BaseCustomerObject" .PHP_EOL;
+$simpleCustomer = new BaseCustomer();
+clientCode($simpleCustomer,$product);
+
+echo PHP_EOL . PHP_EOL;
+
+echo "But it can support a decorated Customer aswell" . PHP_EOL;
+$customerWithHistoric = new BaseCustomerDecorator($simpleCustomer);
+clientCode($customerWithHistoric,$product);
+
+
+
